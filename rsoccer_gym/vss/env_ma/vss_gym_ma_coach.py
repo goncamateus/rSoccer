b32366fd09dbd824df7d50904537a85ac9a50bb0
goncamateus <mgm4@cin.ufpe.br>
2021-06-12 17:21:36 -0300

    explodes ball grad

--- rsoccer_gym/vss/env_ma/vss_gym_ma_coach.py
+++ rsoccer_gym/vss/env_ma/vss_gym_ma_coach.py
@@ -189,7 +189,7 @@ class VSSMACoachEnv(VSSBaseEnv):
         reward = 0
         goal = False
         w_move = 0.05
-        w_ball_grad = 0.8
+        w_ball_grad = 5
         w_energy = 2e-4
         if self.reward_shaping_total is None:
             self.reward_shaping_total = {'goal_score': 0, 'ball_grad': 0,

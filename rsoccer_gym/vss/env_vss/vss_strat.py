108c428556b716082230947815ace339c34f6884
goncamateus <mgm4@cin.ufpe.br>
2021-06-22 15:17:26 -0300

    Tests weights v0

--- rsoccer_gym/vss/env_vss/vss_strat.py
+++ rsoccer_gym/vss/env_vss/vss_strat.py
@@ -67,10 +67,10 @@ class VSSStratEnv(VSSBaseEnv):
         self.actions: Dict = None
         self.reward_shaping_total = None
         self.v_wheel_deadzone = 0.05
-        self.weights = np.array([0.2, 0.8, 2e-4, 10])
         self.max_energy = 93
         self.max_grad = 2.4
         self.max_move = 1.2
+        self.weights = np.array([0.2, 0.75, 0.05, 10])
 
 
         self.ou_actions = []

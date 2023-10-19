f1d5940cba27ca2261c640ecdccc2a086aad7375
goncamateus <mgm4@cin.ufpe.br>
2023-10-19 12:16:24 -0300

    Incremental is wrong

--- agent.py
+++ agent.py
@@ -1,9 +1,9 @@
 import gym
 import rsoccer_gym
 import numpy as np
-from rsoccer_gym.ssl.ssl_path_planning import SSLPathPlanningEnv
+from rsoccer_gym.ssl.ssl_path_planning import SSLPathPlanningEnv, IncrementalPlanningEnv
 
-env = SSLPathPlanningEnv()
+env = IncrementalPlanningEnv()
 for i in range(10):
     env.reset()
     env.render()
@@ -13,8 +13,8 @@ for i in range(10):
         action = np.array(
             [
                 # 0,
-                env.target_point.x / (env.field.length / 2),
-                env.target_point.y / (env.field.width / 2),
+                1,
+                1,
                 np.sin(env.target_angle),
                 np.cos(env.target_angle),
                 # env.target_velocity.x / 2.5,
--- rsoccer_gym/ssl/ssl_path_planning/incremental_planning.py
+++ rsoccer_gym/ssl/ssl_path_planning/incremental_planning.py
@@ -1,5 +1,8 @@
 import numpy as np
-from rsoccer_gym.ssl.ssl_path_planning.ssl_path_planning import SSLPathPlanningEnv
+
+from rsoccer_gym.ssl.ssl_path_planning.ssl_path_planning import (
+    ANGLE_TOLERANCE, DIST_TOLERANCE, Point2D, SSLPathPlanningEnv,
+    abs_smallest_angle_diff, dist_to)
 
 
 class IncrementalPlanningEnv(SSLPathPlanningEnv):
@@ -7,8 +10,6 @@ class IncrementalPlanningEnv(SSLPathPlanningEnv):
         robot = self.frame.robots_blue[0]
         field_half_length = self.field.length / 2  # x
         field_half_width = self.field.width / 2  # y
-        dist = self.time_step * self.max_v * 10
-        action[:2] = action[:2] * dist
         action[0] = action[0] + robot.x
         action[0] = np.clip(action[0], -field_half_length, field_half_length)
         action[0] = action[0] / field_half_length
@@ -16,3 +17,18 @@ class IncrementalPlanningEnv(SSLPathPlanningEnv):
         action[1] = np.clip(action[1], -field_half_width, field_half_width)
         action[1] = action[1] / field_half_width
         return super()._get_commands(action)
+
+    def _dist_reward(self):
+        robot = self.frame.robots_blue[0]       
+        robot = Point2D(x=robot.x, y=robot.y)
+        actual_dist = dist_to(robot, self.target_point)
+        reward = -actual_dist if actual_dist > DIST_TOLERANCE else 10
+        return reward, actual_dist
+
+    def _angle_reward(self):
+        self.frame.robots_blue[0]
+        action_angle = np.arctan2(self.actual_action[2], self.actual_action[3])
+        target = self.target_angle
+        angle_diff = abs_smallest_angle_diff(action_angle, target)
+        angle_reward = -angle_diff / np.pi if angle_diff > ANGLE_TOLERANCE else 1
+        return angle_reward, angle_diff
